5626fcb66c59cde19d09be528186dad698fe24ff
Ahmed Nuaman <ahmed@ahmednuaman.com>
2015-05-20 21:37:03 +0100

    added token

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var argv = require('yargs').argv,
     GithubApi = require('github'),
     github = new GithubApi({
-      version: "3.0.0",
+      version: '3.0.0',
       debug: true,
-      protocol: "https"
+      protocol: 'https'
     }),
     page = 1,
     username = argv._[0] || 'ahmednuaman',
+    token = argv._[1],
+    punchcards,
     repos;
 
 function fetchRepos (page) {
+  github.authenticate({
+    type: 'oauth',
+    token: token
+  });
   github.repos.getFromUser({
     user: username,
     per_page: 100,
-    page: page
+    page: page,
+    sort: 'pushed',
   }, function (err, res) {
     if (!err) {
       handleRepos(res);
@@ -28,7 +35,37 @@ function handleRepos (res) {
     repos = repos.concat(res);
     fetchRepos(++page);
   } else {
-    console.log(repos.length);
+    findPunchCards();
+  }
+}
+
+function fetchPunchCards (repo) {
+  github.authenticate({
+    type: 'oauth',
+    token: token
+  });
+  github.repos.getStatsPunchCard({
+    user: username,
+    repo: repo.name
+  }, function (err, res) {
+    if (!err) {
+      handlePunchCards(res);
+    }
+  });
+}
+
+function handlePunchCards (res) {
+  punchcards = punchcards || [];
+  punchcards = punchcards.concat(res);
+
+  findPunchCards();
+}
+
+function findPunchCards () {
+  if (repos.length) {
+    fetchPunchCards(repos.pop());
+  } else {
+    console.log(punchcards);
   }
 }
 
